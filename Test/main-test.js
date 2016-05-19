d85b2071ff4e6d9c8657c8d596455eae5969d32f
jiaoqiqi <1176932165@qq.com>
2016-05-19 20:28:17 +0800

    commit lcd

--- Test/main-test.js
+++ Test/main-test.js
@@ -1,3 +1,51 @@
 /**
  * Created by jiaoqiqi on 16-5-11.
  */
+describe('LCD',function () {
+    var LCDNumberIcons;
+    var input;
+
+    beforeEach(function () {
+        LCDNumberIcons =buildLCDNumberIcons();
+        input = 12345;
+    });
+    describe('unit test',function () {
+        describe('splitNumbers',function () {
+
+            it('make correct splited numbers',function () {
+                var numberArray = [1,2,3,4,5];
+                expect(splitNumbers(input)).toEqual(numberArray);
+            });
+        });
+        describe('bulidLCDGrid',function () {
+            var LCDLines;
+            beforeEach(function () {
+                LCDLines = [
+                    '... ._. ._. ... ._. ', 
+                    '..| ._| ._| |_| |_. ', 
+                    '..| |_. ._| ..| ._| '];
+            });
+            it('make correct LCDGrid', function () {
+                var LCDGrid =
+                    '... ._. ._. ... ._. \n' +
+                    '..| ._| ._| |_| |_. \n' +
+                    '..| |_. ._| ..| ._| \n';
+                expect(bulidLCDGrid(LCDLines)).toEqual(LCDGrid);
+            });
+        });
+    });
+
+    describe('integration testing',function () {
+
+        it('make correct numberIcons',function () {
+
+            spyOn(console, 'log');
+            LCD(input);
+            var printLCD =
+                '... ._. ._. ... ._. \n' +
+                '..| ._| ._| |_| |_. \n' +
+                '..| |_. ._| ..| ._| \n';
+            expect(console.log).toHaveBeenCalledWith(printLCD);
+        });
+    });
+});
